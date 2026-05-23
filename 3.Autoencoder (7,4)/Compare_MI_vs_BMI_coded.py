318b9e0b3e694feb78d988bd7b7e75429658b204
Rennan Bezerra <rennan.ketsonbezerra@ufpe.br>
2026-05-23 19:15:55 -0300

    [Fix]: Script de comparação no Autoencoder (7,4).

--- 3.Autoencoder (7,4)/Compare_MI_vs_BMI_coded.py
+++ 3.Autoencoder (7,4)/Compare_MI_vs_BMI_coded.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 
 
-from libs.val_model import train, recover_weights, aval_model, recover_points_model, train_curriculum
+from libs.val_model import recover_weights, aval_model, recover_points_model, train_curriculum
 from libs.topology import Net_Coded
 from libs.model_E2E import End2EndSystem
 from libs.AFF3CT_to_points import txt_to_dict
@@ -52,7 +52,6 @@ NUM_TRAINING_ITER    = 150000
 
 k           = 4          # Bits de informação por símbolo
 n           = 7          # Dimensões reais do símbolo transmitido (I e Q)
-SNRdb_train = 5.0        # SNR de treinamento (dB)
 ebno_dbs    = np.arange(-4, 8, 1)
 
 # Defina aqui os melhores valores de 'a' encontrados nos scripts Compare_BMI_FL.py e Compare_MI_FL.py
@@ -124,19 +123,10 @@ for model_name, info in models_info.items():
         optimizer = tf.keras.optimizers.Adam(learning_rate=lr_schedule)
 
         # Treinamento
-        if model_name == "MI":
-            NUM_TRAINING_ITER = 10000
-            BATCH_SIZE = 8000
-            FORCE_RETRAIN = True
-
         if FORCE_RETRAIN or not os.path.exists(local_weights):
-            if model_name == "BMI":
-                train_curriculum(model_train, snr_start=0.0, snr_end=5.0, snr_step=1.0, patience=3000,
-                                 optimizer=optimizer, epochs=NUM_TRAINING_ITER, batchs=BATCH_SIZE,
-                                 local_weights=local_weights, aval_training=True, steps_for_aval=2500, local_aval=local_aval)
-            else:
-                train(model_train, SNRdb_train, optimizer, NUM_TRAINING_ITER, BATCH_SIZE,
-                    local_weights, aval_training=True, steps_for_aval=2500, local_aval=local_aval)
+            train_curriculum(model_train, snr_start=0.0, snr_end=5.0, snr_step=1.0, patience=3000,
+                             optimizer=optimizer, epochs=NUM_TRAINING_ITER, batchs=BATCH_SIZE,
+                             local_weights=local_weights, aval_training=True, steps_for_aval=2500, local_aval=local_aval)
 
         # Recupera pesos
         model_eval = recover_weights(model_eval, local_weights)
